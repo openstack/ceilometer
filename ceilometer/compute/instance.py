a4a15e732bc65e75eccaea91b7778deab9bba566
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-06-25 19:06:46 -0400

    Use the same instance metadata everywhere
    Update the code that generates the instance metadata used in the pollsters
    so that it includes all of the values pulled from the notification
    messages. This keeps the metadata for an instance consistent for all
    metering messages.
    
    Change-Id: I74534e5236fd6580fad42ae4e1c7321dc880bc8f

---
+++ ceilometer/compute/instance.py
@@ -0,0 +1,71 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2012 New Dream Network, LLC (DreamHost)
+#
+# Author: Doug Hellmann <doug.hellmann@dreamhost.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Common code for working with instances
+"""
+
+INSTANCE_PROPERTIES = [
+    # Identity properties
+    'display_name',
+    'reservation_id',
+    # Type properties
+    'architecture'
+    # Location properties
+    'availability_zone',
+    # Image properties
+    'image_ref',
+    'image_ref_url',
+    'kernel_id',
+    'os_type',
+    'ramdisk_id',
+    # Capacity properties
+    'disk_gb',
+    'ephemeral_gb',
+    'memory_mb',
+    'root_gb',
+    'vcpus',
+    ]
+
+
+def get_metadata_from_event(body):
+    """Return a metadata dictionary for the instance mentioned in the
+    notification event.
+    """
+    instance = body['payload']
+    metadata = {
+        'event_type': body['event_type'],
+        'instance_type': instance['instance_type_id'],
+        'host': body['publisher_id'],
+        }
+    for name in INSTANCE_PROPERTIES:
+        metadata[name] = instance.get(name, u'')
+    return metadata
+
+
+def get_metadata_from_dbobject(instance):
+    """Return a metadata dictionary for the instance.
+    """
+    metadata = {
+        'display_name': instance.display_name,
+        'instance_type': (instance.instance_type.flavorid
+                          if instance.instance_type
+                          else None),
+        'host': instance.host,
+        }
+    for name in INSTANCE_PROPERTIES:
+        metadata[name] = instance.get(name, u'')
+    return metadata
---
+++ tests/compute/test_instance.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2012 New Dream Network, LLC (DreamHost)
+#
+# Author: Doug Hellmann <doug.hellmann@dreamhost.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Tests for ceilometer.compute.instance
+"""
+
+from nova import context
+from nova import flags
+from nova import test
+from nova import db
+
+from ceilometer.compute import instance
+from ceilometer.agent import manager
+
+
+class TestLocationMetadata(test.TestCase):
+
+    def setUp(self):
+        self.context = context.RequestContext('admin', 'admin', is_admin=True)
+        self.manager = manager.AgentManager()
+        super(TestLocationMetadata, self).setUp()
+        self.instance = db.instance_create(self.context, {})
+
+    def test_metadata(self):
+        md = instance.get_metadata_from_dbobject(self.instance)
+        for name in instance.INSTANCE_PROPERTIES:
+            assert name in md
