5cb2f9caba3ea9fb55d5964bfb311d027f0e1a3a
Angus Salkeld <asalkeld@redhat.com>
2013-05-01 18:16:48 +1000

    Update oslo before bringing in exceptions
    I want to bring in the base OpenstackException for
    the alarm api, so first updating what we currently have.
    
    Change-Id: I8128f3420eac9ab42a9d679c7b291bac1f27e5b0

--- ceilometer/openstack/common/network_utils.py
+++ ceilometer/openstack/common/network_utils.py
@@ -19,7 +19,8 @@
 Network-related utilities and helper functions.
 """
 
-import logging
+from ceilometer.openstack.common import log as logging
+
 
 LOG = logging.getLogger(__name__)
 
--- ceilometer/openstack/common/rpc/__init__.py
+++ ceilometer/openstack/common/rpc/__init__.py
@@ -26,13 +26,13 @@ For some wrappers that add message versioning to rpc, see:
 """
 
 import inspect
-import logging
 
 from oslo.config import cfg
 
 from ceilometer.openstack.common.gettextutils import _
 from ceilometer.openstack.common import importutils
 from ceilometer.openstack.common import local
+from ceilometer.openstack.common import log as logging
 
 
 LOG = logging.getLogger(__name__)
