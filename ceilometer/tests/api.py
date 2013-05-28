ff94b0e6ed99469fec6830b0fb5c0aa3ee667195
Mark McLoughlin <markmc@redhat.com>
2013-05-28 11:18:13 +0100

    Don't use pecan to configure logging
    Fixes bug #1184941
    
    openstack.common.log gives a mechanism to configure logging and, by
    default, we will set up a handler on root logger. Using pecan to also
    configure a handler on the 'ironic' logger means every log message gets
    logged to console twice.
    
    It seems that the pecan logging configuration stuff is redundant with
    our existing logging configuration infrastructure.
    
    Change-Id: Iec0630b0e842e3b6a90021bf552d9b857c5a0552
    (cherry picked from commit e6a083fc5acb6f3ad03cc1df0943e4e746629128)

--- ceilometer/tests/api.py
+++ ceilometer/tests/api.py
@@ -93,36 +93,12 @@ class FunctionalTest(db_test_base.TestBase):
                                    )
 
         self.config = {
-
             'app': {
                 'root': 'ceilometer.api.controllers.root.RootController',
                 'modules': ['ceilometer.api'],
                 'static_root': '%s/public' % root_dir,
                 'template_path': '%s/ceilometer/api/templates' % root_dir,
                 'enable_acl': enable_acl,
-            },
-
-            'logging': {
-                'loggers': {
-                    'root': {'level': 'INFO', 'handlers': ['console']},
-                    'wsme': {'level': 'INFO', 'handlers': ['console']},
-                    'ceilometer': {'level': 'DEBUG',
-                                   'handlers': ['console'],
-                                   },
-                },
-                'handlers': {
-                    'console': {
-                        'level': 'DEBUG',
-                        'class': 'logging.StreamHandler',
-                        'formatter': 'simple'
-                    }
-                },
-                'formatters': {
-                    'simple': {
-                        'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
-                                   '[%(threadName)s] %(message)s')
-                    }
-                },
             },
         }
 
