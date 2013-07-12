74c1b0d7aab403cd41f8cae77e233c6b91341d6b
Eoghan Glynn <eglynn@redhat.com>
2013-07-12 15:52:44 +0000

    Emit cpu_util from transformer instead of pollster
    The emission of the cpu_util counter from the compute CPUUtilPollster
    can be superceeded by the RateOfChangeTransformer (the intended
    target usercase for the new transformer).
    
    The caching of the CPUStats is now no longer required.
    
    Note that the pipeline.yaml must be updated in deployments, otherwise
    the cpu_util meter will no longer be collected.
    
    Change-Id: I79ff047595b960de7a2acb2ec81c357fe2ee21e5

--- ceilometer/compute/pollsters/cpu.py
+++ ceilometer/compute/pollsters/cpu.py
@@ -18,9 +18,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import abc
-import datetime
-
 from ceilometer import counter
 from ceilometer.compute import plugin
 from ceilometer.compute.pollsters import util
@@ -29,91 +26,25 @@ from ceilometer.openstack.common import log
 LOG = log.getLogger(__name__)
 
 
-class _Base(plugin.ComputePollster):
-
-    CACHE_KEY_CPU = 'cpu'
-
-    def _get_cpu_info(self, inspector, instance_name, cache):
-        i_cache = cache.setdefault(self.CACHE_KEY_CPU, {})
-        if instance_name not in i_cache:
-            i_cache[instance_name] = inspector.inspect_cpus(instance_name)
-        return i_cache[instance_name]
-
-    @abc.abstractmethod
-    def _get_counter(instance, instance_name, cpu_info):
-        """Return one Counter."""
+class CPUPollster(plugin.ComputePollster):
 
     def get_counters(self, manager, cache, instance):
         LOG.info('checking instance %s', instance.id)
         instance_name = util.instance_name(instance)
         try:
-            cpu_info = self._get_cpu_info(
-                manager.inspector,
-                instance_name,
-                cache,
-            )
-            yield self._get_counter(
+            cpu_info = manager.inspector.inspect_cpus(instance_name)
+            LOG.info("CPUTIME USAGE: %s %d",
+                     instance.__dict__, cpu_info.time)
+            cpu_num = {'cpu_number': cpu_info.number}
+            yield util.make_counter_from_instance(
                 instance,
-                instance_name,
-                cpu_info,
+                name='cpu',
+                type=counter.TYPE_CUMULATIVE,
+                unit='ns',
+                volume=cpu_info.time,
+                additional_metadata=cpu_num,
             )
         except Exception as err:
             LOG.error('could not get CPU time for %s: %s',
                       instance.id, err)
             LOG.exception(err)
-
-
-class CPUPollster(_Base):
-
-    @staticmethod
-    def _get_counter(instance, instance_name, cpu_info):
-        LOG.info("CPUTIME USAGE: %s %d",
-                 instance.__dict__, cpu_info.time)
-        cpu_num = {'cpu_number': cpu_info.number}
-        return util.make_counter_from_instance(
-            instance,
-            name='cpu',
-            type=counter.TYPE_CUMULATIVE,
-            unit='ns',
-            volume=cpu_info.time,
-            additional_metadata=cpu_num,
-        )
-
-
-class CPUUtilPollster(_Base):
-    # FIXME(eglynn): once we have a way of configuring which measures
-    #                are published to each sink, we should by default
-    #                disable publishing this derived measure to the
-    #                metering store, only publishing to those sinks
-    #                that specifically need it
-
-    utilization_map = {}
-
-    def _get_cpu_util(self, instance, cpu_info):
-        prev_times = self.utilization_map.get(instance.id)
-        self.utilization_map[instance.id] = (cpu_info.time,
-                                             datetime.datetime.now())
-        cpu_util = 0.0
-        if prev_times:
-            prev_cpu = prev_times[0]
-            prev_timestamp = prev_times[1]
-            delta = self.utilization_map[instance.id][1] - prev_timestamp
-            elapsed = (delta.seconds * (10 ** 6) + delta.microseconds) * 1000
-            cores_fraction = 1.0 / cpu_info.number
-            # account for cpu_time being reset when the instance is restarted
-            time_used = (cpu_info.time - prev_cpu
-                         if prev_cpu <= cpu_info.time else cpu_info.time)
-            cpu_util = 100 * cores_fraction * time_used / elapsed
-        return cpu_util
-
-    def _get_counter(self, instance, instance_name, cpu_info):
-        cpu_util = self._get_cpu_util(instance, cpu_info)
-        LOG.info("CPU UTILIZATION %%: %s %0.2f",
-                 instance.__dict__, cpu_util)
-        return util.make_counter_from_instance(
-            instance,
-            name='cpu_util',
-            type=counter.TYPE_GAUGE,
-            unit='%',
-            volume=cpu_util,
-        )
