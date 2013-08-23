4e72e0273ff8fb5de0214ab0ed24e7239f5592d1
Lianhao Lu <lianhao.lu@intel.com>
2013-08-23 16:28:02 +0800

    Added hardware agent's inspector and snmp implementation
    Added the inspector interface to hardware agent, also added the snmp
    implementation of that inspector.
    
    Implements: blueprint monitoring-physical-devices
    
    Based on the original code at https://github.com/graflu0/ceilometer
    contributed by Lucas and Toni.
    
    Change-Id: I7cbaf94d72d389dc802c42b6ca8939b81a1d6b10
    Signed-off-by: Lucas Graf <graflu0@students.zhaw.ch>
    Signed-off-by: Toni Zehnder <zehndton@students.zhaw.ch>
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

---
+++ ceilometer/hardware/inspector/base.py
@@ -0,0 +1,111 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2014 ZHAW SoE
+#
+# Authors: Lucas Graf <graflu0@students.zhaw.ch>
+#          Toni Zehnder <zehndton@students.zhaw.ch>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Inspector abstraction for read-only access to hardware components"""
+
+import abc
+import collections
+
+import six
+
+# Named tuple representing CPU statistics.
+#
+# cpu1MinLoad: 1 minute load
+# cpu5MinLoad: 5 minute load
+# cpu15MinLoad: 15 minute load
+#
+CPUStats = collections.namedtuple(
+    'CPUStats',
+    ['cpu_1_min', 'cpu_5_min', 'cpu_15_min'])
+
+# Named tuple representing RAM statistics.
+#
+# total: Total Memory (bytes)
+# used: Used Memory (bytes)
+#
+MemoryStats = collections.namedtuple('MemoryStats', ['total', 'used'])
+
+# Named tuple representing disks.
+#
+# device: the device name for the disk
+# path: the path from the disk
+#
+Disk = collections.namedtuple('Disk', ['device', 'path'])
+
+# Named tuple representing disk statistics.
+#
+# size: storage size (bytes)
+# used: storage used (bytes)
+#
+DiskStats = collections.namedtuple('DiskStats', ['size', 'used'])
+
+
+# Named tuple representing an interface.
+#
+# name: the name of the interface
+# mac: the MAC of the interface
+# ip: the IP of the interface
+#
+Interface = collections.namedtuple('Interface', ['name', 'mac', 'ip'])
+
+
+# Named tuple representing network interface statistics.
+#
+# bandwidth: current bandwidth (bytes/s)
+# rx_bytes: total number of octets received (bytes)
+# tx_bytes: total number of octets transmitted (bytes)
+# error: number of outbound packets not transmitted because of errors
+#
+InterfaceStats = collections.namedtuple(
+    'InterfaceStats',
+    ['bandwidth', 'rx_bytes', 'tx_bytes', 'error'])
+
+
+@six.add_metaclass(abc.ABCMeta)
+class Inspector(object):
+    @abc.abstractmethod
+    def inspect_cpu(self, host):
+        """Inspect the CPU statistics for a host.
+
+        :param host: the target host
+        :return: iterator of CPUStats
+        """
+
+    @abc.abstractmethod
+    def inspect_disk(self, host):
+        """Inspect the disk statistics for a host.
+
+        :param : the target host
+        :return: iterator of tuple (Disk, DiskStats)
+        """
+
+    @abc.abstractmethod
+    def inspect_memory(self, host):
+        """Inspect the ram statistics for a host.
+
+        :param : the target host
+        :return: iterator of MemoryStats
+        """
+
+    @abc.abstractmethod
+    def inspect_network(self, host):
+        """Inspect the network interfaces for a host.
+
+        :param : the target host
+        :return: iterator of tuple (Interface, InterfaceStats)
+        """
---
+++ ceilometer/tests/hardware/inspector/base.py
@@ -0,0 +1,63 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2014 Intel Corp
+#
+# Authors: Lianhao Lu <lianhao.lu@intel.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from ceilometer.hardware.inspector import base
+
+
+class InspectorBaseTest(object):
+    """Subclass must set self.inspector and self.host in
+    self.setUp()
+    """
+
+    cpu = [base.CPUStats(cpu_1_min=0.1,
+                         cpu_5_min=0.2,
+                         cpu_15_min=0.3),
+           ]
+
+    network = [(base.Interface(name='eth0',
+                               mac='112233445566',
+                               ip='10.0.0.1'),
+                base.InterfaceStats(bandwidth=1250000 / 8,
+                                    rx_bytes=1000,
+                                    tx_bytes=2000,
+                                    error=1)),
+               ]
+    diskspace = [(base.Disk(device='/dev/sda1', path='/'),
+                  base.DiskStats(size=1000, used=500),
+                  ),
+                 (base.Disk(device='/dev/sda2', path='/home'),
+                  base.DiskStats(size=2000, used=1000),
+                  ),
+                 ]
+    memory = [base.MemoryStats(total=1000, used=500)]
+
+    def test_inspect_cpu(self):
+        self.assertEqual(list(self.inspector.inspect_cpu(self.host)),
+                         self.cpu)
+
+    def test_inspect_network(self):
+        self.assertEqual(list(self.inspector.inspect_network(self.host)),
+                         self.network)
+
+    def test_inspect_disk(self):
+        self.assertEqual(list(self.inspector.inspect_disk(self.host)),
+                         self.diskspace)
+
+    def test_inspect_memory(self):
+        self.assertEqual(list(self.inspector.inspect_memory(self.host)),
+                         self.memory)
