e626c9ca2835c61192168f1d33c6d93551ddd57a
Alexei Kornienko <alexei.kornienko@gmail.com>
2014-02-09 16:36:07 +0200

    Refactored run-tests script
    Refactored run-tests script to become a setup script.
    This is needed to make it work correctly with arbitary command we want
    to run in venv (testenv:venv in tox).
    
    Change-Id: I91d0e7a05fb1b9fa13e18789281fdf5267f5b526

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -8,12 +8,7 @@ function clean_exit(){
     return $error_code
 }
 
-if [ "$1" = "--coverage" ]; then
-	COVERAGE_ARG="$1"
-	shift
-fi
-
-# Main unit tests
+# Setup MongoDB test server
 MONGO_DATA=`mktemp -d /tmp/CEILO-MONGODB-XXXXX`
 MONGO_PORT=29000
 trap "clean_exit" EXIT
@@ -31,7 +26,8 @@ do
     echo "$line" | grep -q "waiting for connections on port ${MONGO_PORT}" && break
 done < ${MONGO_DATA}/out
 # Read the fifo for ever otherwise mongod would block
-# + that gives us the log on screen
 cat ${MONGO_DATA}/out > /dev/null &
 export CEILOMETER_TEST_MONGODB_URL="mongodb://localhost:${MONGO_PORT}/ceilometer"
-python setup.py testr --slowest --testr-args="$*" $COVERAGE_ARG
+
+# Yield execution to venv command
+$*
