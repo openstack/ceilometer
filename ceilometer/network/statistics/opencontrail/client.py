6e0f4d9bd9c7f3b957adc6f73bf1a48c8c120e1b
Sylvain Afchain <sylvain.afchain@enovance.com>
2014-04-05 07:43:55 +0200

    Opencontrail network statistics driver
    This patch introduces a network statistics driver
    for Opencontrail. Only port statistics are currently
    returned by the driver.
    
    Implements: blueprint meter-from-opencontrail
    Co-Authored-By: Edouard Thuleau <edouard.thuleau@cloudwatt.com>
    Change-Id: Ic0afc478362fb4170903ee4e3723b82cd6c723fa

---
+++ ceilometer/network/statistics/opencontrail/client.py
@@ -0,0 +1,165 @@
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Sylvain Afchain <sylvain.afchain@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo.config import cfg
+import requests
+import six
+from six.moves.urllib import parse as url_parse
+
+from ceilometer.openstack.common.gettextutils import _
+from ceilometer.openstack.common import log
+
+
+CONF = cfg.CONF
+
+
+LOG = log.getLogger(__name__)
+
+
+class OpencontrailAPIFailed(Exception):
+    pass
+
+
+class AnalyticsAPIBaseClient(object):
+    """Opencontrail Base Statistics REST API Client."""
+
+    def __init__(self, endpoint, username, password, domain, verify_ssl=True):
+        self.endpoint = endpoint
+        self.username = username
+        self.password = password
+        self.domain = domain
+        self.verify_ssl = verify_ssl
+        self.sid = None
+
+    def authenticate(self):
+        path = '/authenticate'
+        data = {'username': self.username,
+                'password': self.password,
+                'domain': self.domain}
+
+        req_params = self._get_req_params(data=data)
+        url = url_parse.urljoin(self.endpoint, path)
+        resp = requests.post(url, **req_params)
+        if resp.status_code != 302:
+            raise OpencontrailAPIFailed(
+                _('Opencontrail API returned %(status)s %(reason)s') %
+                {'status': resp.status_code, 'reason': resp.reason})
+        self.sid = resp.cookies['connect.sid']
+
+    def request(self, path, fqdn_uuid, data, retry=True):
+        if not self.sid:
+            self.authenticate()
+
+        if not data:
+            data = {'fqnUUID': fqdn_uuid}
+        else:
+            data['fqnUUID'] = fqdn_uuid
+
+        req_params = self._get_req_params(data=data,
+                                          cookies={'connect.sid': self.sid})
+
+        url = url_parse.urljoin(self.endpoint, path)
+        self._log_req(url, req_params)
+        resp = requests.get(url, **req_params)
+        self._log_res(resp)
+
+        # it seems that the sid token has to be renewed
+        if resp.status_code == 302:
+            self.sid = 0
+            if retry:
+                return self.request(path, fqdn_uuid, data,
+                                    retry=False)
+
+        if resp.status_code != 200:
+            raise OpencontrailAPIFailed(
+                _('Opencontrail API returned %(status)s %(reason)s') %
+                {'status': resp.status_code, 'reason': resp.reason})
+
+        return resp
+
+    def _get_req_params(self, params=None, data=None, cookies=None):
+        req_params = {
+            'headers': {
+                'Accept': 'application/json'
+            },
+            'data': data,
+            'verify': self.verify_ssl,
+            'allow_redirects': False,
+            'cookies': cookies
+        }
+
+        return req_params
+
+    @staticmethod
+    def _log_req(url, req_params):
+        if not CONF.debug:
+            return
+
+        curl_command = ['REQ: curl -i -X GET ']
+
+        params = []
+        for name, value in six.iteritems(req_params['data']):
+            params.append("%s=%s" % (name, value))
+
+        curl_command.append('"%s?%s" ' % (url, '&'.join(params)))
+
+        for name, value in six.iteritems(req_params['headers']):
+            curl_command.append('-H "%s: %s" ' % (name, value))
+
+        LOG.debug(''.join(curl_command))
+
+    @staticmethod
+    def _log_res(resp):
+        if not CONF.debug:
+            return
+
+        dump = ['RES: \n']
+        dump.append('HTTP %.1f %s %s\n' % (resp.raw.version,
+                                           resp.status_code,
+                                           resp.reason))
+        dump.extend(['%s: %s\n' % (k, v)
+                     for k, v in six.iteritems(resp.headers)])
+        dump.append('\n')
+        if resp.content:
+            dump.extend([resp.content, '\n'])
+
+        LOG.debug(''.join(dump))
+
+
+class NetworksAPIClient(AnalyticsAPIBaseClient):
+    """Opencontrail Statistics REST API Client."""
+
+    def get_port_statistics(self, fqdn_uuid):
+        """Get port statistics of a network
+
+        URL:
+            /tenant/networking/virtual-machines/details
+        PARAMS:
+            fqdnUUID=fqdn_uuid
+            type=vn
+        """
+
+        path = '/api/tenant/networking/virtual-machines/details'
+        resp = self.request(path, fqdn_uuid, {'type': 'vn'})
+
+        return resp.json()
+
+
+class Client(object):
+
+    def __init__(self, endpoint, username, password, domain, verify_ssl=True):
+        self.networks = NetworksAPIClient(endpoint, username, password,
+                                          domain, verify_ssl)
---
+++ ceilometer/network/statistics/opencontrail/driver.py
@@ -0,0 +1,149 @@
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Sylvain Afchain <sylvain.afchain@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from six.moves.urllib import parse as url_parse
+
+from ceilometer.network.statistics import driver
+from ceilometer.network.statistics.opencontrail import client
+from ceilometer import neutron_client
+from ceilometer.openstack.common import timeutils
+
+
+class OpencontrailDriver(driver.Driver):
+    """Driver of network analytics of Opencontrail.
+
+    This driver uses resources in "pipeline.yaml".
+    Resource requires below conditions:
+    * resource is url
+    * scheme is "opencontrail"
+
+    This driver can be configured via query parameters.
+    Supported parameters:
+    * scheme:
+        The scheme of request url to Opencontrail Analytics endpoint.
+        (default http)
+    * username:
+        This is username used by Opencontrail Analytics.(default None)
+    * password:
+        This is password used by Opencontrail Analytics.(default None)
+    * domain
+        This is domain used by Opencontrail Analytics.(default None)
+    * verify_ssl
+        Specify if the certificate will be checked for https request.
+        (default false)
+
+    e.g.
+        opencontrail://localhost:8143/?username=admin&password=admin&
+        scheme=https&domain=&verify_ssl=true
+    """
+    @staticmethod
+    def _prepare_cache(endpoint, params, cache):
+
+        if 'network.statistics.opencontrail' in cache:
+            return cache['network.statistics.opencontrail']
+
+        data = {
+            'o_client': client.Client(endpoint,
+                                      params['username'],
+                                      params['password'],
+                                      params.get('domain'),
+                                      params.get('verify_ssl') == 'true'),
+            'n_client': neutron_client.Client()
+        }
+
+        cache['network.statistics.opencontrail'] = data
+
+        return data
+
+    def get_sample_data(self, meter_name, parse_url, params, cache):
+
+        parts = url_parse.ParseResult(params.get('scheme', ['http'])[0],
+                                      parse_url.netloc,
+                                      parse_url.path,
+                                      None,
+                                      None,
+                                      None)
+        endpoint = url_parse.urlunparse(parts)
+
+        iter = self._get_iter(meter_name)
+        if iter is None:
+            # The extractor for this meter is not implemented or the API
+            # doesn't have method to get this meter.
+            return
+
+        extractor = self._get_extractor(meter_name)
+        if extractor is None:
+            # The extractor for this meter is not implemented or the API
+            # doesn't have method to get this meter.
+            return
+
+        data = self._prepare_cache(endpoint, params, cache)
+
+        ports = data['n_client'].port_get_all()
+        ports_map = dict((port['id'], port['tenant_id']) for port in ports)
+
+        networks = data['n_client'].network_get_all()
+
+        for network in networks:
+            net_id = network['id']
+
+            timestamp = timeutils.utcnow().isoformat()
+            statistics = data['o_client'].networks.get_port_statistics(net_id)
+            if not statistics:
+                continue
+
+            for value in statistics['value']:
+                for sample in iter(extractor, value, ports_map):
+                    if sample is not None:
+                        sample[2]['network_id'] = net_id
+                        yield sample + (timestamp, )
+
+    def _get_iter(self, meter_name):
+        if meter_name.startswith('switch.port'):
+            return self._iter_port
+
+    def _get_extractor(self, meter_name):
+        method_name = '_' + meter_name.replace('.', '_')
+        return getattr(self, method_name, None)
+
+    @staticmethod
+    def _iter_port(extractor, value, ports_map):
+        ifstats = value['value']['UveVirtualMachineAgent']['if_stats_list']
+        for ifstat in ifstats:
+            name = ifstat['name']
+            device_owner_id, port_id = name.split(':')
+
+            tenant_id = ports_map.get(port_id)
+
+            resource_meta = {'device_owner_id': device_owner_id,
+                             'tenant_id': tenant_id}
+            yield extractor(ifstat, port_id, resource_meta)
+
+    @staticmethod
+    def _switch_port_receive_packets(statistic, resource_id, resource_meta):
+        return (int(statistic['in_pkts']), resource_id, resource_meta)
+
+    @staticmethod
+    def _switch_port_transmit_packets(statistic, resource_id, resource_meta):
+        return (int(statistic['out_pkts']), resource_id, resource_meta)
+
+    @staticmethod
+    def _switch_port_receive_bytes(statistic, resource_id, resource_meta):
+        return (int(statistic['in_bytes']), resource_id, resource_meta)
+
+    @staticmethod
+    def _switch_port_transmit_bytes(statistic, resource_id, resource_meta):
+        return (int(statistic['out_bytes']), resource_id, resource_meta)
