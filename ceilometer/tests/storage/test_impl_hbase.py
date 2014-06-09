7fc557986577856a8ac67b967226e6c48604924e
Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
2014-06-09 11:13:00 +0200

    Splits hbase storage code base
    The hbase storage file have three different kind of things
    * The Connection class
    * The In-Memory implementation of HBase
    * The a list of helper methods
    
    This change splits this three things into separate files
    
    Partial implements blueprint dedicated-alarm-database
    Co-Authored-By: Ala Rezmerita <ala.rezmerita@cloudwatt.com>
    
    Change-Id: I6453dc7795fb24d8ea773b129874e7936befae47

--- ceilometer/tests/storage/test_impl_hbase.py
+++ ceilometer/tests/storage/test_impl_hbase.py
@@ -25,6 +25,7 @@
 """
 from mock import patch
 
+from ceilometer.storage.hbase import inmemory as hbase_inmemory
 from ceilometer.storage import impl_hbase as hbase
 from ceilometer.tests import base as test_base
 from ceilometer.tests import db as tests_db
@@ -36,7 +37,8 @@ class ConnectionTest(tests_db.TestBase,
     @tests_db.run_with('hbase')
     def test_hbase_connection(self):
         conn = hbase.Connection(self.db_manager.url)
-        self.assertIsInstance(conn.conn_pool.connection(), hbase.MConnection)
+        self.assertIsInstance(conn.conn_pool.connection(),
+                              hbase_inmemory.MConnection)
 
         class TestConn(object):
             def __init__(self, host, port):
