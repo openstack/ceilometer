ab0c18e5d4074085e2006e3daec902c018a23b8a
Nejc Saje <nsaje@redhat.com>
2014-07-21 09:06:36 +0200

    Separate the console scripts
    Having all the console scripts in one file means that we always pull
    in all the dependencies. This presents a problem in downstream packaging,
    for example, a distro alarm subpackage required pymongo dependencies
    installed, even if not used.
    
    This patch separates the console scripts into a 'cmd' module, the
    way Nova does it. Eventlet monkey patching is done in the module's
    __init__.py file.
    
    Co-Authored-By: Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
    Closes-bug: #1317210
    Change-Id: I83020b13670928fd0d3046d2d2a493b11fe81d61

--- ceilometer/cli.py
+++ ceilometer/cli.py
@@ -30,95 +30,12 @@ import eventlet
 eventlet.monkey_patch(socket=True, select=True, thread=True)
 from oslo.config import cfg
 
-from ceilometer.alarm import service as alarm_service
-from ceilometer.api import app
-from ceilometer.central import manager as central_manager
-from ceilometer import collector
-from ceilometer.compute import manager as compute_manager
-from ceilometer import notification
 from ceilometer.openstack.common import context
-from ceilometer.openstack.common import importutils
-from ceilometer.openstack.common import service as os_service
 from ceilometer.openstack.common import timeutils
 from ceilometer import pipeline
 from ceilometer import sample
 from ceilometer import service
-from ceilometer import storage
 from ceilometer import transformer
-
-
-OPTS = [
-    cfg.StrOpt('evaluation_service',
-               default='ceilometer.alarm.service.SingletonAlarmService',
-               help='Class to launch as alarm evaluation service.'),
-]
-
-cfg.CONF.register_opts(OPTS, group='alarm')
-cfg.CONF.import_opt('time_to_live', 'ceilometer.storage',
-                    group='database')
-
-LOG = logging.getLogger(__name__)
-
-
-def alarm_notifier():
-    service.prepare_service()
-    os_service.launch(alarm_service.AlarmNotifierService()).wait()
-
-
-def alarm_evaluator():
-    service.prepare_service()
-    eval_service = importutils.import_object(cfg.CONF.alarm.evaluation_service)
-    os_service.launch(eval_service).wait()
-
-
-def agent_central():
-    service.prepare_service()
-    os_service.launch(central_manager.AgentManager()).wait()
-
-
-def agent_compute():
-    service.prepare_service()
-    os_service.launch(compute_manager.AgentManager()).wait()
-
-
-def agent_notification():
-    service.prepare_service()
-    launcher = os_service.ProcessLauncher()
-    launcher.launch_service(
-        notification.NotificationService(),
-        workers=service.get_workers('notification'))
-    launcher.wait()
-
-
-def api():
-    service.prepare_service()
-    srv = app.build_server()
-    srv.serve_forever()
-
-
-def collector_service():
-    service.prepare_service()
-    launcher = os_service.ProcessLauncher()
-    launcher.launch_service(
-        collector.CollectorService(),
-        workers=service.get_workers('collector'))
-    launcher.wait()
-
-
-def storage_dbsync():
-    service.prepare_service()
-    storage.get_connection_from_config(cfg.CONF).upgrade()
-
-
-def storage_expirer():
-    service.prepare_service()
-    if cfg.CONF.database.time_to_live > 0:
-        LOG.debug(_("Clearing expired metering data"))
-        storage_conn = storage.get_connection_from_config(cfg.CONF)
-        storage_conn.clear_expired_metering_data(
-            cfg.CONF.database.time_to_live)
-    else:
-        LOG.info(_("Nothing to clean, database time to live is disabled"))
 
 
 def send_sample():
