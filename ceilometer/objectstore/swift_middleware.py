9d4e233eb75d4b69208e89504e1fb94547d87832
gordon chung <gord@live.ca>
2014-09-10 14:59:35 -0400

    ceilometer spamming syslog
    regression in code which causes ceilometer to spam syslog. this
    patch explicitly configures log_level in ceilometer swift
    middleware as we no longer rely on swift middleware logic to
    configure it.
    
    Change-Id: I248040cb1d842cf89ed32ef28d8575012d36e08f
    Closes-Bug: #1367880

--- ceilometer/objectstore/swift_middleware.py
+++ ceilometer/objectstore/swift_middleware.py
@@ -40,17 +40,14 @@ before "proxy-server" and add the following filter in the file:
 """
 
 from __future__ import absolute_import
+import logging
 
 from oslo.utils import timeutils
 
 from ceilometer.openstack.common import context
-from ceilometer.openstack.common import log
 from ceilometer import pipeline
 from ceilometer import sample
 from ceilometer import service
-
-
-LOG = log.getLogger(__name__)
 
 
 class InputProxy(object):
@@ -94,6 +91,9 @@ class CeilometerMiddleware(object):
                                      "metadata_headers",
                                      "").split(",") if h.strip()]
 
+        self.logger = logging.getLogger('ceilometer')
+        self.logger.setLevel(getattr(logging,
+                                     conf.get('log_level', 'WARN').upper()))
         service.prepare_service([])
 
         self.pipeline_manager = pipeline.setup_pipeline()
@@ -132,7 +132,7 @@ class CeilometerMiddleware(object):
                                         input_proxy.bytes_received,
                                         bytes_sent)
                 except Exception:
-                    LOG.exception('Failed to publish samples')
+                    self.logger.exception('Failed to publish samples')
 
         try:
             iterable = self.app(env, my_start_response)
