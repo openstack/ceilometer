c8f4e4bef6b633d7c42ac24ed1ef7926fa4d3ac0
liu-sheng <liusheng@huawei.com>
2015-09-07 15:37:03 +0800

    Use b64encode to replace of encodestring
    The encodestring() method is used to encode a string, and return a string
    containing one or more lines of base64-encoded data. For meter_id, the
    return is same as b64encode() method except an extra trailing newline
    ('\n').
    
    Change-Id: Ia14747ff0b4c643b33ed99da7fa422480f0f8095
    Closes-Bug: #1271530

--- ceilometer/tests/functional/api/v2/test_list_meters_scenarios.py
+++ ceilometer/tests/functional/api/v2/test_list_meters_scenarios.py
@@ -785,7 +785,11 @@ class TestListMeters(v2.FunctionalTest,
         data = self.get_json('/meters')
         for i in data:
             meter_id = '%s+%s' % (i['resource_id'], i['name'])
-            expected = base64.encodestring(meter_id.encode('utf-8'))
+            expected = base64.b64encode(meter_id.encode('utf-8'))
             if six.PY3:
                 expected = expected.decode('ascii')
             self.assertEqual(expected, i['meter_id'])
+            self.assertFalse(i['meter_id'].endswith('\n'))
+            self.assertEqual(
+                [i['resource_id'].encode('utf-8'), i['name'].encode('utf-8')],
+                base64.b64decode(i['meter_id']).split('+'))
