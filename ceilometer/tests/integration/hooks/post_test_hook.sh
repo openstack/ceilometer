8b9a6bc9847c06bc09bbb90b9c1d4b1bce5a2f49
gordon chung <gord@live.ca>
2016-01-29 13:40:00 -0500

    integration-gate: fix publicURL retrieval
    The output changed from publicURL to public.
    
    Related-Bug: #1539728
    Change-Id: Iaf84421dd13a23e6f3fd0b4cb61ec7de4e3ad30c

--- ceilometer/tests/integration/hooks/post_test_hook.sh
+++ ceilometer/tests/integration/hooks/post_test_hook.sh
@@ -39,10 +39,10 @@ if [ -d $BASE/new/devstack ]; then
 fi
 
 openstack catalog list
-export AODH_SERVICE_URL=$(openstack catalog show alarming -c endpoints -f value | awk '/publicURL/{print $2}')
-export GNOCCHI_SERVICE_URL=$(openstack catalog show metric -c endpoints -f value | awk '/publicURL/{print $2}')
-export HEAT_SERVICE_URL=$(openstack catalog show orchestration -c endpoints -f value | awk '/publicURL/{print $2}')
-export NOVA_SERVICE_URL=$(openstack catalog show compute -c endpoints -f value | awk '/publicURL/{print $2}')
+export AODH_SERVICE_URL=$(openstack catalog show alarming -c endpoints -f value | awk '/public/{print $2}')
+export GNOCCHI_SERVICE_URL=$(openstack catalog show metric -c endpoints -f value | awk '/public/{print $2}')
+export HEAT_SERVICE_URL=$(openstack catalog show orchestration -c endpoints -f value | awk '/public/{print $2}')
+export NOVA_SERVICE_URL=$(openstack catalog show compute -c endpoints -f value | awk '/public/{print $2}')
 export GLANCE_IMAGE_NAME=$(openstack image list | awk '/ cirros.*uec /{print $4}')
 export ADMIN_TOKEN=$(openstack token issue -c id -f value)
 
