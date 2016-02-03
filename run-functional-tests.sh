36c5b87058618162b5924e0dcc268eda04e83be0
Julien Danjou <julien@danjou.info>
2016-02-03 15:56:02 +0100

    Use overtest to setup functional backends
    This patches leverage overtest to setup the database environment needed.
    
    Change-Id: I8198041b4a87bf187d8f6499f0c7c1a43d4dcac2
    Depends-On: I29e480611e743ddc645fd067ca2c63b51b11b518

--- run-functional-tests.sh
+++ run-functional-tests.sh
@@ -2,11 +2,10 @@
 set -e
 # Use a mongodb backend by default
 
-
 if [ -z $CEILOMETER_TEST_BACKEND ]; then
     CEILOMETER_TEST_BACKEND="mongodb"
 fi
-echo $CEILOMETER_TEST_BACKEND
+
 for backend in $CEILOMETER_TEST_BACKEND; do
-    ./setup-test-env-${backend}.sh ./tools/pretty_tox.sh $*
+    overtest $backend ./tools/pretty_tox.sh $*
 done
