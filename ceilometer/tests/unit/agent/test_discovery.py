e4ece33b0f3d4ccaf54be5af9844d516cd7696ab
ChangBo Guo(gcb) <eric.guo@easystack.cn>
2016-06-28 17:43:17 +0800

    test: Fix wrong override value of config option interface
    Config option interface with parameter choices = [public, internal,
    admin, auth, publicURL, internalURL, adminURL], any other value will
    raise ValueError.
    
    Change-Id: Ia2b3b51a5a5a3357ea8520e797849f39f1cef13b

--- ceilometer/tests/unit/agent/test_discovery.py
+++ ceilometer/tests/unit/agent/test_discovery.py
@@ -31,7 +31,7 @@ class TestEndpointDiscovery(base.BaseTestCase):
         self.discovery = endpoint.EndpointDiscovery()
         self.manager = mock.MagicMock()
         self.CONF = self.useFixture(fixture_config.Config()).conf
-        self.CONF.set_override('interface', 'test-endpoint-type',
+        self.CONF.set_override('interface', 'publicURL',
                                group='service_credentials')
         self.CONF.set_override('region_name', 'test-region-name',
                                group='service_credentials')
@@ -41,14 +41,14 @@ class TestEndpointDiscovery(base.BaseTestCase):
     def test_keystone_called(self):
         self.discovery.discover(self.manager, param='test-service-type')
         expected = [mock.call(service_type='test-service-type',
-                              interface='test-endpoint-type',
+                              interface='publicURL',
                               region_name='test-region-name')]
         self.assertEqual(expected, self.catalog.get_urls.call_args_list)
 
     def test_keystone_called_no_service_type(self):
         self.discovery.discover(self.manager)
         expected = [mock.call(service_type=None,
-                              interface='test-endpoint-type',
+                              interface='publicURL',
                               region_name='test-region-name')]
         self.assertEqual(expected,
                          self.catalog.get_urls
