f2279a62226eb37a7b4e735be3c7a877d0006472
Huan Xie <huan.xie@citrix.com>
2017-05-08 19:55:22 -0700

    XenAPI: use os-xenapi for XenAPI driver
    os-xenapi contains all XenServer dom0 plugins and provide utility
    tools for communicating to XenServer dom0 plugins, currently both
    nova and neutron projects are using os-xenapi. This patch is to
    change ceilometer to use os-xenapi too.
    
    Change-Id: I14819fd57305edb0f4337af88e56ed6b980d7da8

--- ceilometer/compute/virt/xenapi/inspector.py
+++ ceilometer/compute/virt/xenapi/inspector.py
@@ -13,22 +13,17 @@
 # under the License.
 """Implementation of Inspector abstraction for XenAPI."""
 
+from os_xenapi.client import session as xenapi_session
+from os_xenapi.client import XenAPI
 from oslo_config import cfg
+from oslo_log import log as logging
 from oslo_utils import units
-import six.moves.urllib.parse as urlparse
-try:
-    import XenAPI as api
-except ImportError:
-    api = None
-
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
 
 from ceilometer.compute.pollsters import util
 from ceilometer.compute.virt import inspector as virt_inspector
 from ceilometer.i18n import _
+
+LOG = logging.getLogger(__name__)
 
 opt_group = cfg.OptGroup(name='xenapi',
                          title='Options for XenAPI')
@@ -50,22 +45,7 @@ class XenapiException(virt_inspector.InspectorException):
     pass
 
 
-def swap_xapi_host(url, host_addr):
-    """Replace the XenServer address present in 'url' with 'host_addr'."""
-    temp_url = urlparse.urlparse(url)
-    # The connection URL is served by XAPI and doesn't support having a
-    # path for the connection url after the port. And username/password
-    # will be pass separately. So the URL like "http://abc:abc@abc:433/abc"
-    # should not appear for XAPI case.
-    temp_netloc = temp_url.netloc.replace(temp_url.hostname, '%s' % host_addr)
-    replaced = temp_url._replace(netloc=temp_netloc)
-    return urlparse.urlunparse(replaced)
-
-
 def get_api_session(conf):
-    if not api:
-        raise ImportError(_('XenAPI not installed'))
-
     url = conf.xenapi.connection_url
     username = conf.xenapi.connection_username
     password = conf.xenapi.connection_password
@@ -74,22 +54,12 @@ def get_api_session(conf):
                                 'connection_password to use'))
 
     try:
-        session = (api.xapi_local() if url == 'unix://local'
-                   else api.Session(url))
-        session.login_with_password(username, password)
-    except api.Failure as e:
-        if e.details[0] == 'HOST_IS_SLAVE':
-            master = e.details[1]
-            url = swap_xapi_host(url, master)
-            try:
-                session = api.Session(url)
-                session.login_with_password(username, password)
-            except api.Failure as es:
-                raise XenapiException(_('Could not connect slave host: %s') %
-                                      es.details[0])
-        else:
-            msg = _("Could not connect to XenAPI: %s") % e.details[0]
-            raise XenapiException(msg)
+        session = xenapi_session.XenAPISession(url, username, password,
+                                               originator="ceilometer")
+        LOG.debug("XenAPI session is created successfully, %s", session)
+    except XenAPI.Failure as e:
+        msg = _("Could not connect to XenAPI: %s") % e.details[0]
+        raise XenapiException(msg)
     return session
 
 
@@ -98,31 +68,9 @@ class XenapiInspector(virt_inspector.Inspector):
     def __init__(self, conf):
         super(XenapiInspector, self).__init__(conf)
         self.session = get_api_session(self.conf)
-        self.host_ref = self._get_host_ref()
-        self.host_uuid = self._get_host_uuid()
-
-    def _get_host_ref(self):
-        """Return the xenapi host on which nova-compute runs on."""
-        return self.session.xenapi.session.get_this_host(self.session.handle)
-
-    def _get_host_uuid(self):
-        return self.session.xenapi.host.get_uuid(self.host_ref)
-
-    def _call_xenapi(self, method, *args):
-        return self.session.xenapi_request(method, args)
-
-    def _call_plugin(self, plugin, fn, args):
-        args['host_uuid'] = self.host_uuid
-        return self.session.xenapi.host.call_plugin(
-            self.host_ref, plugin, fn, args)
-
-    def _call_plugin_serialized(self, plugin, fn, *args, **kwargs):
-        params = {'params': pickle.dumps(dict(args=args, kwargs=kwargs))}
-        rv = self._call_plugin(plugin, fn, params)
-        return pickle.loads(rv)
 
     def _lookup_by_name(self, instance_name):
-        vm_refs = self._call_xenapi("VM.get_by_name_label", instance_name)
+        vm_refs = self.session.VM.get_by_name_label(instance_name)
         n = len(vm_refs)
         if n == 0:
             raise virt_inspector.InstanceNotFoundException(
@@ -138,30 +86,28 @@ class XenapiInspector(virt_inspector.Inspector):
         vm_ref = self._lookup_by_name(instance_name)
         cpu_util = self._get_cpu_usage(vm_ref, instance_name)
         memory_usage = self._get_memory_usage(vm_ref)
+        LOG.debug("inspect_instance, cpu_util: %(cpu)s, memory_usage: %(mem)s",
+                  {'cpu': cpu_util, 'mem': memory_usage}, instance=instance)
         return virt_inspector.InstanceStats(cpu_util=cpu_util,
                                             memory_usage=memory_usage)
 
     def _get_cpu_usage(self, vm_ref, instance_name):
-        vcpus_number = int(self._call_xenapi("VM.get_VCPUs_max", vm_ref))
+        vcpus_number = int(self.session.VM.get_VCPUs_max(vm_ref))
         if vcpus_number <= 0:
             msg = _("Could not get VM %s CPU number") % instance_name
             raise XenapiException(msg)
         cpu_util = 0.0
         for index in range(vcpus_number):
-            cpu_util += float(self._call_xenapi("VM.query_data_source",
-                                                vm_ref,
-                                                "cpu%d" % index))
+            cpu_util += float(self.session.VM.query_data_source(
+                vm_ref, "cpu%d" % index))
         return cpu_util / int(vcpus_number) * 100
 
     def _get_memory_usage(self, vm_ref):
-        total_mem = float(self._call_xenapi("VM.query_data_source",
-                                            vm_ref,
-                                            "memory"))
+        total_mem = float(self.session.VM.query_data_source(vm_ref, "memory"))
         try:
-            free_mem = float(self._call_xenapi("VM.query_data_source",
-                                               vm_ref,
-                                               "memory_internal_free"))
-        except api.Failure:
+            free_mem = float(self.session.VM.query_data_source(
+                vm_ref, "memory_internal_free"))
+        except XenAPI.Failure:
             # If PV tools is not installed in the guest instance, it's
             # impossible to get free memory. So give it a default value
             # as 0.
@@ -174,12 +120,15 @@ class XenapiInspector(virt_inspector.Inspector):
     def inspect_vnics(self, instance, duration):
         instance_name = util.instance_name(instance)
         vm_ref = self._lookup_by_name(instance_name)
-        dom_id = self._call_xenapi("VM.get_domid", vm_ref)
-        vif_refs = self._call_xenapi("VM.get_VIFs", vm_ref)
-        bw_all = self._call_plugin_serialized('bandwidth',
-                                              'fetch_all_bandwidth')
+        dom_id = self.session.VM.get_domid(vm_ref)
+        vif_refs = self.session.VM.get_VIFs(vm_ref)
+        bw_all = self.session.call_plugin_serialized('bandwidth',
+                                                     'fetch_all_bandwidth')
+        LOG.debug("inspect_vnics, all bandwidth: %s", bw_all,
+                  instance=instance)
+
         for vif_ref in vif_refs or []:
-            vif_rec = self._call_xenapi("VIF.get_record", vif_ref)
+            vif_rec = self.session.VIF.get_record(vif_ref)
 
             bw_vif = bw_all[dom_id][vif_rec['device']]
 
@@ -197,17 +146,15 @@ class XenapiInspector(virt_inspector.Inspector):
     def inspect_vnic_rates(self, instance, duration):
         instance_name = util.instance_name(instance)
         vm_ref = self._lookup_by_name(instance_name)
-        vif_refs = self._call_xenapi("VM.get_VIFs", vm_ref)
+        vif_refs = self.session.VM.get_VIFs(vm_ref)
         if vif_refs:
             for vif_ref in vif_refs:
-                vif_rec = self._call_xenapi("VIF.get_record", vif_ref)
+                vif_rec = self.session.VIF.get_record(vif_ref)
 
-                rx_rate = float(self._call_xenapi(
-                    "VM.query_data_source", vm_ref,
-                    "vif_%s_rx" % vif_rec['device']))
-                tx_rate = float(self._call_xenapi(
-                    "VM.query_data_source", vm_ref,
-                    "vif_%s_tx" % vif_rec['device']))
+                rx_rate = float(self.session.VM.query_data_source(
+                    vm_ref, "vif_%s_rx" % vif_rec['device']))
+                tx_rate = float(self.session.VM.query_data_source(
+                    vm_ref, "vif_%s_tx" % vif_rec['device']))
 
                 yield virt_inspector.InterfaceRateStats(
                     name=vif_rec['uuid'],
@@ -220,17 +167,15 @@ class XenapiInspector(virt_inspector.Inspector):
     def inspect_disk_rates(self, instance, duration):
         instance_name = util.instance_name(instance)
         vm_ref = self._lookup_by_name(instance_name)
-        vbd_refs = self._call_xenapi("VM.get_VBDs", vm_ref)
+        vbd_refs = self.session.VM.get_VBDs(vm_ref)
         if vbd_refs:
             for vbd_ref in vbd_refs:
-                vbd_rec = self._call_xenapi("VBD.get_record", vbd_ref)
+                vbd_rec = self.session.VBD.get_record(vbd_ref)
 
-                read_rate = float(self._call_xenapi(
-                    "VM.query_data_source", vm_ref,
-                    "vbd_%s_read" % vbd_rec['device']))
-                write_rate = float(self._call_xenapi(
-                    "VM.query_data_source", vm_ref,
-                    "vbd_%s_write" % vbd_rec['device']))
+                read_rate = float(self.session.VM.query_data_source(
+                    vm_ref, "vbd_%s_read" % vbd_rec['device']))
+                write_rate = float(self.session.VM.query_data_source(
+                    vm_ref, "vbd_%s_write" % vbd_rec['device']))
                 yield virt_inspector.DiskRateStats(
                     device=vbd_rec['device'],
                     read_bytes_rate=read_rate,
