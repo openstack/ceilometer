3d0b48133984d35a6a07c5e9963792faf961cc72
Julien Danjou <julien@danjou.info>
2018-09-18 11:09:06 +0200

    event: remove deprecated method
    Change-Id: Iadda5e51d61c44c4dd11d7fc82bc7938f74baa5c

--- ceilometer/tests/unit/meter/test_meter_plugins.py
+++ ceilometer/tests/unit/meter/test_meter_plugins.py
@@ -27,13 +27,13 @@ class TestTimedeltaPlugin(base.BaseTestCase):
     def test_timedelta_transformation(self):
         match_list = [('test.timestamp1', '2016-03-02T15:04:32'),
                       ('test.timestamp2', '2016-03-02T16:04:32')]
-        value = self.plugin.trait_value(match_list)
-        self.assertEqual(3600, value)
+        value = self.plugin.trait_values(match_list)
+        self.assertEqual([3600], value)
 
     def test_timedelta_missing_field(self):
         match_list = [('test.timestamp1', '2016-03-02T15:04:32')]
-        with mock.patch('%s.LOG' % self.plugin.trait_value.__module__) as log:
-            self.assertIsNone(self.plugin.trait_value(match_list))
+        with mock.patch('%s.LOG' % self.plugin.trait_values.__module__) as log:
+            self.assertEqual([None], self.plugin.trait_values(match_list))
             log.warning.assert_called_once_with(
                 'Timedelta plugin is required two timestamp fields to create '
                 'timedelta value.')
@@ -42,8 +42,8 @@ class TestTimedeltaPlugin(base.BaseTestCase):
         match_list = [('test.timestamp1', '2016-03-02T15:04:32'),
                       ('test.timestamp2', '2016-03-02T16:04:32'),
                       ('test.timestamp3', '2016-03-02T16:10:32')]
-        with mock.patch('%s.LOG' % self.plugin.trait_value.__module__) as log:
-            self.assertIsNone(self.plugin.trait_value(match_list))
+        with mock.patch('%s.LOG' % self.plugin.trait_values.__module__) as log:
+            self.assertEqual([None], self.plugin.trait_values(match_list))
             log.warning.assert_called_once_with(
                 'Timedelta plugin is required two timestamp fields to create '
                 'timedelta value.')
@@ -51,8 +51,8 @@ class TestTimedeltaPlugin(base.BaseTestCase):
     def test_timedelta_invalid_timestamp(self):
         match_list = [('test.timestamp1', '2016-03-02T15:04:32'),
                       ('test.timestamp2', '2016-03-02T15:004:32')]
-        with mock.patch('%s.LOG' % self.plugin.trait_value.__module__) as log:
-            self.assertIsNone(self.plugin.trait_value(match_list))
+        with mock.patch('%s.LOG' % self.plugin.trait_values.__module__) as log:
+            self.assertEqual([None], self.plugin.trait_values(match_list))
             msg = log.warning._mock_call_args[0][0]
             self.assertTrue(msg.startswith('Failed to parse date from set '
                                            'fields, both fields ')
@@ -61,11 +61,11 @@ class TestTimedeltaPlugin(base.BaseTestCase):
     def test_timedelta_reverse_timestamp_order(self):
         match_list = [('test.timestamp1', '2016-03-02T15:15:32'),
                       ('test.timestamp2', '2016-03-02T15:10:32')]
-        value = self.plugin.trait_value(match_list)
-        self.assertEqual(300, value)
+        value = self.plugin.trait_values(match_list)
+        self.assertEqual([300], value)
 
     def test_timedelta_precise_difference(self):
         match_list = [('test.timestamp1', '2016-03-02T15:10:32.786893'),
                       ('test.timestamp2', '2016-03-02T15:10:32.786899')]
-        value = self.plugin.trait_value(match_list)
-        self.assertEqual(0.000006, value)
+        value = self.plugin.trait_values(match_list)
+        self.assertEqual([0.000006], value)
