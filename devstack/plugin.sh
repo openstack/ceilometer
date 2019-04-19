56fd8168d236f504358558fa45fd9ad68f3ffe52
zhurong <aaronzhu1121@gmail.com>
2019-04-19 13:31:05 +0800

    tempest: Allow to configure tempest config
    Change-Id: Ied0822ee1031c399c7aa86482e8f570c48c3935e

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -404,9 +404,9 @@ if is_service_enabled ceilometer; then
         start_ceilometer
     elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
         iniset $TEMPEST_CONFIG telemetry alarm_granularity $CEILOMETER_ALARM_GRANULARITY
-        iniset $TEMPEST_CONFIG telemetry alarm_threshold 10000000
-        iniset $TEMPEST_CONFIG telemetry alarm_metric_name cpu
-        iniset $TEMPEST_CONFIG telemetry alarm_aggregation_method rate:mean
+        iniset $TEMPEST_CONFIG telemetry alarm_threshold $CEILOMETER_ALARM_THRESHOLD
+        iniset $TEMPEST_CONFIG telemetry alarm_metric_name $CEILOMETER_ALARM_METRIC_NAME
+        iniset $TEMPEST_CONFIG telemetry alarm_aggregation_method $CEILOMETER_ALARM_AGGREGATION_METHOD
     fi
 
     if [[ "$1" == "unstack" ]]; then
