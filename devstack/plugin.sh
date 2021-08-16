openstack/ceilometer

commit f57a51e43ca7a8903763cc24b1c510fa50a16b2c

Do not install libvirt-python on RHEL via pip
since that is already in place.

Closes-Bug: #1896327
Depends-on: https://review.opendev.org/c/openstack/telemetry-tempest-plugin/+/801038

Change-Id: Ic158561b3f4d8ddcc092f4e8da1b3215f976fc98

devstack/plugin.sh
@@ -97,7 +97,7 @@ function _ceilometer_prepare_coordination {
 function _ceilometer_prepare_virt_drivers {
     # Only install virt drivers if we're running nova compute
     if is_service_enabled n-cpu ; then
-        if [[ "$VIRT_DRIVER" = 'libvirt' ]]; then
+        if [[ "$VIRT_DRIVER" = 'libvirt' && "$DISTRO" != 'rhel8' ]]; then
             pip_install_gr libvirt-python
         fi
 

.zuul.yaml
@@ -25,6 +25,13 @@
       - ^releasenotes/.*$
       - ^ceilometer/tests/.*$
 
+- job:
+    name: telemetry-dsvm-integration-centos-8s
+    nodeset: devstack-single-node-centos-8-stream
+    parent: telemetry-dsvm-integration
+    description: |
+      Functional testing for a Centos 8 stream system
+
 - project:
     templates:
       - openstack-cover-jobs
@@ -36,6 +43,8 @@
     check:
       jobs:
         - grenade-ceilometer
+        - telemetry-dsvm-integration-centos-8s:
+            irrelevant-files: *ceilometer-irrelevant-files
         - telemetry-dsvm-integration:
             irrelevant-files: *ceilometer-irrelevant-files
         - telemetry-dsvm-integration-ipv6-only:
@@ -44,6 +53,8 @@
       queue: telemetry
       jobs:
         - grenade-ceilometer
+        - telemetry-dsvm-integration-centos-8s:
+            irrelevant-files: *ceilometer-irrelevant-files
         - telemetry-dsvm-integration:
             irrelevant-files: *ceilometer-irrelevant-files
         - telemetry-dsvm-integration-ipv6-only:

commit 1b97560c97d8b06ca5568b909ae69eb07d0415fc

Do not install libvirt python bindings from pip
Since [1] was merged, devstack no longer installs libvirt python
bindings from pip but use distro packages. This change removes
the logic to install the python bindings using pip to follow that
change in devstack, otherwise installation fails because of conflict
between packages available in pip and one installed by distro packages.

[1] https://review.opendev.org/c/openstack/devstack/+/798514

Conflicts:
	devstack/plugin.sh

Resolved conflicts caused by f57a51e43ca7a8903763cc24b1c510fa50a16b2c
which is not backported to stable branches.

Closes-Bug: #1939990
Related-Bug: #1933096
Change-Id: I369a95cf30e91738d10ad2e5a3d5f6988bdd76cb
(cherry picked from commit 9366bc8f18a07695aa1af9217307b0a55376a331)

devstack/plugin.sh
@@ -97,9 +97,8 @@ function _ceilometer_prepare_coordination {
 function _ceilometer_prepare_virt_drivers {
     # Only install virt drivers if we're running nova compute
     if is_service_enabled n-cpu ; then
-        if [[ "$VIRT_DRIVER" = 'libvirt' ]]; then
-            pip_install_gr libvirt-python
-        fi
+        # NOTE(tkajinam): pythonN-libvirt is installed using distro
+        # packages in devstack
 
         if [[ "$VIRT_DRIVER" = 'vsphere' ]]; then
             pip_install_gr oslo.vmware