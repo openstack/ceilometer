44a2711bfdcd2497b933921232e22ed46268c0a9
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-12-18 21:02:25 +0900

    doc: Drop DJANGO_SETTINGS_MODULE environment
    ... because django is not relevant to ceilometer.
    
    Change-Id: Ia0bcc7dda26c8632fe1f932946268bfeb5bb18dc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,9 +20,6 @@ ROOT = os.path.abspath(os.path.join(BASE_DIR, "..", ".."))
 sys.path.insert(0, ROOT)
 sys.path.insert(0, BASE_DIR)
 
-# This is required for ReadTheDocs.org, but isn't a bad idea anyway.
-os.environ['DJANGO_SETTINGS_MODULE'] = 'openstack_dashboard.settings'
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
