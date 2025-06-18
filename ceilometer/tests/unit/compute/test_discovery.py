openstack/ceilometer

commit 663c742ffcb49253e2d3dd9779629d10b2d8f4c6

Ensure libvirt python binding is installed for tests
Current unit tests expect that the libvirt python binding is installed
and available in the environment.

Change-Id: I278a4cae66bca5becb0777a0c90ec8092c2e8180

ceilometer/tests/unit/compute/test_discovery.py
@@ -14,19 +14,13 @@ import datetime
 from unittest import mock
 
 import fixtures
-import testtools
-
+import libvirt
 from novaclient import exceptions
-
-try:
-    import libvirt
-except ImportError:
-    libvirt = None
 
 from ceilometer.compute import discovery
 from ceilometer.compute.pollsters import util
 from ceilometer import service
-import ceilometer.tests.base as base
+from ceilometer.tests import base
 
 
 LIBVIRT_METADATA_XML = """
@@ -101,6 +95,9 @@ class FakeConn:
     def listAllDomains(self):
         return [FakeDomain()]
 
+    def isAlive(self):
+        return True
+
 
 class FakeManualInstanceDomain:
     def state(self):
@@ -129,7 +126,7 @@ class FakeManualInstanceConn:
         return [FakeManualInstanceDomain()]
 
     def isAlive(self):
-        return False
+        return True
 
 
 class TestDiscovery(base.BaseTestCase):
@@ -217,14 +214,13 @@ class TestDiscovery(base.BaseTestCase):
         self.client.instance_get_all_by_host.assert_called_once_with(
             self.CONF.host, "2016-01-01T00:00:00+00:00")
 
-    @testtools.skipUnless(libvirt, "libvirt not available")
-    @mock.patch.object(libvirt, "VIR_DOMAIN_METADATA_ELEMENT", 2)
-    @mock.patch.object(libvirt, "openReadOnly")
-    def test_discovery_with_libvirt(self, openReadOnly):
+    @mock.patch("ceilometer.compute.virt.libvirt.utils."
+                "refresh_libvirt_connection")
+    def test_discovery_with_libvirt(self, mock_libvirt_conn):
         self.CONF.set_override("instance_discovery_method",
                                "libvirt_metadata",
                                group="compute")
-        openReadOnly.return_value = FakeConn()
+        mock_libvirt_conn.return_value = FakeConn()
         dsc = discovery.InstanceDiscovery(self.CONF)
         resources = dsc.discover(mock.MagicMock())
 
@@ -288,22 +284,18 @@ class TestDiscovery(base.BaseTestCase):
         self.assertEqual(expected_calls,
                          self.client.instance_get_all_by_host.call_args_list)
 
-    @testtools.skipUnless(libvirt, "libvirt not available")
-    @mock.patch.object(libvirt, "VIR_DOMAIN_METADATA_ELEMENT", 2)
-    @mock.patch.object(libvirt, "openReadOnly")
-    def test_discovery_with_libvirt_error(self, openReadOnly):
+    @mock.patch("ceilometer.compute.virt.libvirt.utils."
+                "refresh_libvirt_connection")
+    def test_discovery_with_libvirt_error(self, mock_libvirt_conn):
         self.CONF.set_override("instance_discovery_method",
                                "libvirt_metadata",
                                group="compute")
-        openReadOnly.return_value = FakeManualInstanceConn()
+        mock_libvirt_conn.return_value = FakeManualInstanceConn()
         dsc = discovery.InstanceDiscovery(self.CONF)
         resources = dsc.discover(mock.MagicMock())
         self.assertEqual(0, len(resources))
 
     def test_get_server(self):
-        self.CONF.set_override("instance_discovery_method",
-                               "libvirt_metadata",
-                               group="compute")
         self.client.nova_client = mock.MagicMock()
         self.client.nova_client.servers = mock.MagicMock()
 

commit a068bcd1812572781efbbb568e46316e2a17464e

Fix incomplete mock of libvirtError
... to resolve the frequent unit tests failures caused by
663c742ffcb49253e2d3dd9779629d10b2d8f4c6 .

Change-Id: Iaa4e6a2aef52d2594fe8fce9271bc2a823d46f7b

ceilometer/tests/unit/compute/test_discovery.py
@@ -117,8 +117,14 @@ class FakeManualInstanceDomain:
         # elements like: '<nova:instance
         #  xmlns:nova="http://openstack.org/xmlns/libvirt/nova/1.0">'
         # When invoke get metadata method, raise libvirtError.
-        raise libvirt.libvirtError(
+        e = libvirt.libvirtError(
             "metadata not found: Requested metadata element is not present")
+
+        def fake_error_code(*args, **kwargs):
+            return libvirt.VIR_ERR_NO_DOMAIN_METADATA
+
+        e.get_error_code = fake_error_code
+        raise e
 
 
 class FakeManualInstanceConn: