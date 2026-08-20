1145d02929faeff8298e4ae6d6c1df77a02c7134
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-08-21 00:06:48 +0900

    Remove invalid endpoint type
    Keystone supports only public, internal and admin.
    
    Change-Id: I6bd24e3731168887e4083e41858a86a966988a5e
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- ceilometer/keystone_client.py
+++ ceilometer/keystone_client.py
@@ -254,8 +254,8 @@ CLI_OPTS = [
                    'OS_INTERFACE', os.environ.get('OS_ENDPOINT_TYPE',
                                                   'public')),
                deprecated_name="os-endpoint-type",
-               choices=('public', 'internal', 'admin', 'auth', 'publicURL',
-                        'internalURL', 'adminURL'),
+               choices=('public', 'internal', 'admin',
+                        'publicURL', 'internalURL', 'adminURL'),
                help='Type of endpoint in Identity service catalog to use for '
                     'communication with OpenStack services.'),
 ]
